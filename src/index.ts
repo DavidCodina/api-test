34e2a21837309553d9ec91850acb05e0077321e4
David Codina <davidmcodina@icloud.com>
2023-05-22 10:19:54 -0600

    Made CI jobs sequential

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import testRoutes from './routes/testRoutes'
 dotenv.config()
 const app = express()
 
-let message = 'Whuddup!'
+let message = 'Whuddup!!!'
 console.log(message)
 
 ///////////////////////////////////////////////////////////////////////////
