6569c778054dee0d4c8909aa70d6dbd95aab8da0
David Codina <davidmcodina@icloud.com>
2023-05-22 11:27:29 -0600

    asdfasd

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,7 @@ app.get('/', (req, res) => {
   const body = req.body || {}
 
   return res.status(200).json({
-    message: 'CI Test 3 (NO CI)',
+    message: 'CI Test',
     envTest: process.env.TEST,
     body: body
   })
